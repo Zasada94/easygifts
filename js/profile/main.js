79f80aa9edd3a821fd0e14284f5cd833d041467d
Zasada94 <pawelzasada2@wp.pl>
2023-09-22 10:17:08 +0200

    logged in apla finished

--- js/profile/main.js
+++ js/profile/main.js
@@ -4,3 +4,10 @@ const menuWrapper = document.getElementById("menuWrapper");
 menuButton.addEventListener("click", () => {
 	menuWrapper.classList.toggle("active");
 });
+
+const loginTrigger = document.querySelector(".btn-signin");
+const apla = document.querySelector(".login_apla");
+
+loginTrigger.addEventListener("click", () => {
+	apla.classList.toggle("active");
+});
